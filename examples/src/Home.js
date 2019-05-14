87688cf4c3dd851a3ff6c36e3a81d482d49abea5
Jimseung <lannex.shin@gmail.com>
2019-05-14 11:43:13 +0900

    Add examples

---
+++ examples/src/Home.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import OverflowWrapper from '../../dist';
+
+class Home extends Component {
+  render() {
+    const styles = {
+      box: {
+        border: 'solid 1px #ddd',
+      },
+      wrapper: {
+        height: 26,
+      },
+      item: {
+        margin: '0 10px',
+      },
+    };
+    const items = [
+      'apple',
+      'grape',
+      'banana',
+      'melon',
+      'kiwi',
+      'peach',
+      'mango',
+      'tomato',
+      'pineapple',
+      'blueberry',
+      'avocado',
+      'papaya',
+      'pear',
+    ];
+
+    return (
+      <div>
+        <div style={styles.box}>
+          <OverflowWrapper style={styles.wrapper}>
+            {items.map(el => (
+              <div key={el} style={styles.item}>
+                {el}
+              </div>
+            ))}
+          </OverflowWrapper>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
