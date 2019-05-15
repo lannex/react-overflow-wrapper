4467651f89554ac5d46d0c0e54c77a22bf626b30
Jimseung <lannex.shin@gmail.com>
2019-05-15 10:52:34 +0900

    Add ESlint in the examples

--- examples/src/Home.js
+++ examples/src/Home.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import OverflowWrapper from '../../dist';
 
-class Home extends Component {
+class Home extends PureComponent {
   render() {
     const styles = {
       box: {
